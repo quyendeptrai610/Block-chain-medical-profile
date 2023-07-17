bbff2979dd45e6719677754b053690ce803be404
lưu minh quyền <luuminhquyen@MacBook-Air-cua-luu.local>
2023-07-17 10:56:54 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaBlackTie, FaTwitter, FaGithub, FaGitlab } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <footer className="footer px-5 lg:px-28 py-10 bg-primary text-white mt-20">
+            <div>
+                <Link
+                    to="/"
+                    className="normal-case text-xl font-bold flex items-center"
+                >
+                    <i>
+                        <FaBlackTie className="text-white mr-1" />
+                    </i>
+                    CariKerja.io
+                </Link>
+                <p>Coded by Fariz Ghani</p>
+            </div>
+            <div>
+                <span className=" text-lg font-semibold">Social</span>
+                <div className="grid grid-flow-col gap-4">
+                    <Link to="https://twitter.com/farizge">
+                        <FaTwitter className="text-2xl hover:text-secondary" />
+                    </Link>
+                    <Link to="https://github.com/farizge">
+                        <FaGithub className="text-2xl hover:text-secondary" />
+                    </Link>
+                    <Link to="https://gitlab.com/farizge">
+                        <FaGitlab className="text-2xl hover:text-secondary" />
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
